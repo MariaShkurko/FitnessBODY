6905c2e6cc62592a6753bbaf7b9cd2222320c76a
Maria Shkurko <shkurko_mashenka@mail.ru>
2021-03-29 14:25:12 +0300

    Create calculator

--- src/modules/calc.js
+++ src/modules/calc.js
@@ -1,5 +1,79 @@
 const calc = () => {
+    if (document.querySelector('html').id === 'main') {
+        const cardOrderForm = document.getElementById('card_order'),
+            inputClub = cardOrderForm.querySelectorAll('.club > input');
 
+        const calculate = priceArr => {
+            const inputTime = cardOrderForm.querySelectorAll('.time > input'),
+                priceTotal = document.getElementById('price-total'),
+                promoCode = cardOrderForm.querySelector('.price-message > input');
+
+            const calcPrice = () => {
+                let priceCurrent = 0;
+
+                inputTime.forEach((item, i) => {
+                    if (item.checked) priceCurrent = priceArr[i];
+                });
+
+                if (promoCode.value.trim() === 'ТЕЛО2020') {
+                    priceCurrent = Math.ceil(priceCurrent * 0.7);
+                }
+
+                priceTotal.textContent = priceCurrent;
+            };
+
+            calcPrice();
+
+            inputTime.forEach(item => {
+                item.addEventListener('change', calcPrice);
+            });
+            promoCode.addEventListener('blur', calcPrice);
+
+        };
+
+        const getPrice = (html, selector) => {
+            const price = html.querySelectorAll(selector),
+                priceArr = [];
+
+            price.forEach((item, i) => {
+                priceArr[i] = item.textContent.slice(0, -1);
+            });
+
+            priceArr[3] = priceArr[4];
+
+            return priceArr;
+        };
+
+        const request = (url, selector) => {
+            const html = document.createElement('html');
+            fetch(url)
+                .then(responce => {
+                    if (responce.status !== 200) {
+                        throw new Error('status network not 200');
+                    }
+                    return responce.text();
+                })
+                .then(responceBody => {
+                    html.innerHTML = responceBody;
+                    return getPrice(html, selector);
+                })
+                .then(priceArr => {
+                    calculate(priceArr);
+                })
+                .catch(error => {
+                    console.error(error);
+                });
+        };
+
+        inputClub.forEach(item => {
+            if (item.checked) request(`/${item.value}.html`, '.cards-types > label > .cost');
+
+            item.addEventListener('change', () => {
+                if (item.checked) request(`/${item.value}.html`, '.cards-types > label > .cost');
+            });
+        });
+
+    }
 };
 
 export default calc;
--- src/modules/sendForm.js
+++ src/modules/sendForm.js
@@ -1,5 +1,80 @@
 const sendForm = errorValidate => {
+    const errorMessage = 'Что-то пошло не так...',
+        successMessage = 'Спасибо! Мы скоро с вами свяжемся!',
+        incorrectDataMessage = 'Введены некоректные данные',
+        loadSpin = document.createElement('div'),
+        loadSpinChild1 = document.createElement('div'),
+        loadSpinChild2 = document.createElement('div'),
+        statusMessage = document.createElement('div');
 
+    statusMessage.classList.add('status-message');
+
+    loadSpin.className = 'sk-wandering-cubes';
+    loadSpinChild1.className = 'sk-cube';
+    loadSpinChild2.className = 'sk-cube';
+    loadSpinChild1.classList.add('sk-cube-1');
+    loadSpinChild2.classList.add('sk-cube-2');
+    loadSpin.insertAdjacentElement('afterbegin', loadSpinChild2);
+    loadSpin.insertAdjacentElement('afterbegin', loadSpinChild1);
+
+    const bannerForm = document.getElementById('banner-form'),
+        cardOrder = document.getElementById('card_order'),
+        footerForm = document.getElementById('footer_form'),
+        callbackForm = document.getElementById('form1'),
+        freeVisitForm = document.getElementById('form2');
+
+    const postData = body => fetch('./server.php', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(body)
+    });
+
+    const prepareData = (event, form) => {
+        event.preventDefault();
+        form.appendChild(statusMessage);
+
+        if (errorValidate.size) {
+            statusMessage.textContent = incorrectDataMessage;
+            return;
+        }
+
+        statusMessage.textContent = '';
+        statusMessage.insertAdjacentElement('afterbegin', loadSpin);
+
+        const body = {},
+            formData = new FormData(form);
+
+        formData.forEach((val, key) => {
+            body[key] = val;
+        });
+
+        postData(body)
+            .then(responce => {
+                if (responce.status !== 200) {
+                    throw new Error('status network not 200');
+                }
+                statusMessage.textContent = successMessage;
+                form.querySelectorAll('input').forEach(item => {
+                    item.value = '';
+                });
+            })
+            .catch(error => {
+                statusMessage.textContent = errorMessage;
+                console.error(error);
+            });
+    };
+
+    bannerForm.addEventListener('submit', event => { prepareData(event, bannerForm); });
+    cardOrder.addEventListener('submit', event => { prepareData(event, cardOrder); });
+    footerForm.addEventListener('submit', event => { prepareData(event, footerForm); });
+    callbackForm.addEventListener('submit', event => {
+        prepareData(event, callbackForm);
+    });
+    freeVisitForm.addEventListener('submit', event => {
+        prepareData(event, freeVisitForm);
+    });
 };
 
 export default sendForm;
